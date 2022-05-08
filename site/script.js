ed0125c580a6dc5f2537efd9dec460b5b0f12d09
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-05-08 19:22:24 +0300

    add script

--- site/script.js
+++ site/script.js
@@ -1,9 +1,35 @@
-var myNodelist = document.getElementsByTagName("LI");
+function addNewTask() {
+  var elem = document.createElement('li');
+  var inputValue = document.getElementById("myInput").value;
+  var text = document.createTextNode(inputValue);
+  elem.appendChild(text);
+
+  if (inputValue === '') 
+  {
+      alert("You must write something!");
+  } 
+  else 
+  {
+      document.getElementById("myList").appendChild(elem);
+  }
+  document.getElementById("myInput").value = "";
+
+  var closeBtn = document.createElement("span");
+  var text = document.createTextNode("\u00D7");
+  closeBtn.className = "closeBtn";
+  closeBtn.appendChild(text);
+  elem.appendChild(closeBtn);
+}
+
+
+
+var listOfElements = document.getElementsByTagName("li");
 var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+for (i = 0; i < listOfElements.length; i++)
+{
+  var closeBtn = document.createElement("span");
+  var text = document.createTextNode("\u00D7");
+  closeBtn.className = "closeBtn";
+  closeBtn.appendChild(text);
+  listOfElements[i].appendChild(closeBtn);
 }
